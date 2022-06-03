72fce42c108d9e07ba0b98740a75f9773cd1d71b
Mohiuddinr22 <88410867+Mohiuddinr22@users.noreply.github.com>
2022-06-03 15:20:54 +0600

    Add files via upload

---
+++ src/ArraysClass/EliminateDuplicates.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int size = 10;
+    int arr[size] = {5,3,5,5,6,7,8,3,7,2};
+    for(int i=0; i<size-1; i++){
+        for(int j=0; j<size; j++){
+            if(i!=j && arr[i]==arr[j]){
+                for(int k=j; k<size; k++){
+                    arr[k] = arr[k+1];
+                }
+                size--;
+            }
+        }
+    }
+
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<endl;
+    }
+
+    return 0;
+}
