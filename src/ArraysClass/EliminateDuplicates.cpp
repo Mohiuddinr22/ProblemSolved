e993895b4d942b3a7af664d89b03dd6e6859745f
Mohiuddinr22 <88410867+Mohiuddinr22@users.noreply.github.com>
2022-06-06 18:25:01 +0600

    Update EliminateDuplicates.cpp

--- src/ArraysClass/EliminateDuplicates.cpp
+++ src/ArraysClass/EliminateDuplicates.cpp
@@ -3,20 +3,22 @@ using namespace std;
 
 int main(){
     int size = 10;
-    int arr[size] = {5,3,5,5,6,7,8,3,7,2};
+    int array[size] = {1,9,1,9,1,9,1,9,1,9};
     for(int i=0; i<size; i++){
         for(int j=0; j<size; j++){
-            if(i!=j && arr[i]==arr[j]){
-                for(int k=j; k<size; k++){
-                    arr[k] = arr[k+1];
+            if(i!=j && array[i]==array[j]){
+                while(array[i]==array[j]){
+                    for(int k=j; k<size; k++)
+                        array[k] = array[k+1];
+
+                    size--;
                 }
-                size--;
             }
         }
     }
 
     for(int i=0; i<size; i++){
-        cout<<arr[i]<<endl;
+        cout<<array[i]<<endl;
     }
 
     return 0;
