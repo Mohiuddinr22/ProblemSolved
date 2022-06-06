1b817f9d0eda45d771788016cf827425556f1ef5
Mohiuddinr22 <88410867+Mohiuddinr22@users.noreply.github.com>
2022-06-06 18:30:58 +0600

    Update OccuranceForEveryElement.cpp

--- src/ArraysClass/OccuranceForEveryElement.cpp
+++ src/ArraysClass/OccuranceForEveryElement.cpp
@@ -2,34 +2,36 @@
 using namespace std;
 
 int main(){
-    int sizeOfArray = 10;
-    int arr[sizeOfArray] = {3,4,6,2,3,3,5,2,1,6};
-    int newArr[] = {3,4,6,2,3,3,5,2,1,6};
-    for(int i=0; i<(sizeOfArray-1); i++){
-        for(int j=0; j<sizeOfArray; j++){
-            if(i!=j && arr[i]==arr[j]){
-                for(int k=j; k<sizeOfArray; k++){
-                    arr[k] = arr[k+1];
+    int size = 10;
+    int array[size] = {1,1,1,1,1,1,1,1,1,1};
+    int newArray[] = {1,1,1,1,1,1,1,1,1,1};
+    for(int i=0; i<size; i++){
+        for(int j=0; j<size; j++){
+            if(i!=j && array[i]==array[j]){
+                while(array[i]==array[j]){
+                    for(int k=j; k<size; k++)
+                        array[k] = array[k+1];
+
+                    size--;
                 }
-                sizeOfArray--;
             }
         }
     }
 
-    int numCount[sizeOfArray];
-    for(int i=0; i<sizeOfArray; i++)
+    int numCount[size];
+    for(int i=0; i<size; i++)
         numCount[i]=0;
 
-    for(int i=0; i<sizeOfArray; i++){
+    for(int i=0; i<size; i++){
         for(int j=0; j<10; j++){
-            if(arr[i]==newArr[j]){
+            if(array[i]==newArray[j]){
                 numCount[i]++;
             }
         }
     }
 
-    for(int i=0; i<sizeOfArray; i++)
-        cout<<arr[i]<<" is = "<<numCount[i]<<" times."<<endl;
+    for(int i=0; i<size; i++)
+        cout<<array[i]<<" is = "<<numCount[i]<<" times."<<endl;
 
     return 0;
 }
