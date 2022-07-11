50c549b584d603d29f1dd05fe02128f2decfaac2
Mohiuddinr22 <88410867+Mohiuddinr22@users.noreply.github.com>
2022-07-11 22:10:46 +0600

    Update LinkedList.cpp

--- src/dsLinkedList/LinkedList.cpp
+++ src/dsLinkedList/LinkedList.cpp
@@ -7,81 +7,182 @@ class Node
 public:
     int data;
     Node *next;
+    Node *previous;
+
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
 
     Node(int item)
     {
-        data = item;
-        next = NULL;
+        this->data = item;
+        this->next = NULL;
     }
 };
 
-void insertAtLast(Node *&head, int item)
+class LinkedList
 {
-    Node *node = new Node(item);
-    if (head == NULL)
+    Node *first;
+    int size = 0;
+
+public:
+    LinkedList()
     {
-        head = node;
+        first = NULL;
+    }
+    void insertAtLast(int item);    // Done
+    void insertAtFirst(int item);   // Done
+    int sizeOfList();               // Done
+    void display();                 // Done
+    void displayArray(int *array);  // Done
+    bool exists(int item);          // Done
+    int valueAtIndex(int index);    // Done
+    int getPreviousValue(int item); // Done
+    int *toArray();                 // Done
+    Node getPrevious(Node *node);
+    void removeFromLast();
+    void removeFromFirst();
+};
+
+void LinkedList::insertAtLast(int item)
+{
+    Node *last = first;
+    Node *node = new Node(item);
+    if (first == NULL)
+    {
+        first = last = node;
+        size++;
         return;
     }
-    Node *tail = head;
-    while (tail->next != NULL)
+    while (last->next != NULL)
     {
-        tail = tail->next;
+        last = last->next;
     }
-    tail->next = node;
+    last->next = node;
+    size++;
 }
 
-void display(Node *head)
+void LinkedList::insertAtFirst(int item)
 {
-    Node *tail = head;
-    while (tail != NULL)
+    Node *last = first;
+    Node *node = new Node(item);
+    if (first == NULL)
     {
-        cout << tail->data << " -> ";
-        tail = tail->next;
+        first = last = node;
+        size++;
+        return;
+    }
+    node->next = first;
+    first = node;
+    size++;
+}
+
+int LinkedList::sizeOfList()
+{
+    return size;
+}
+
+void LinkedList::display()
+{
+    Node *last = first;
+    while (last != NULL)
+    {
+        cout << last->data << " -> ";
+        last = last->next;
     }
     cout << "NULL" << endl;
 }
 
-void insertAtFirst(Node *&head, int item)
+void LinkedList::displayArray(int *array)
 {
-    Node *node = new Node(item);
-    if (head == NULL)
+    cout << "[";
+    for (int i = 0; i < size - 1; i++)
     {
-        head = node;
-        return;
+        cout << array[i] << ", ";
     }
-    node->next = head;
-    head = node;
+    cout << array[size - 1] << "]" << endl;
 }
 
-bool exists(Node *&head, int item)
+bool LinkedList::exists(int item)
 {
-    Node *tail = head;
-    while (tail != NULL)
+    Node *last = first;
+    if (first == NULL)
     {
-        if (tail->data == item)
+        cout << "List is empty" << endl;
+        return false;
+    }
+    while (last != NULL)
+    {
+        if (last->data == item)
         {
             return true;
         }
-        tail = tail->next;
+        last = last->next;
     }
     return false;
 }
 
+int LinkedList::valueAtIndex(int index)
+{
+    if (index < 0 || index >= size)
+    {
+        cout << "Invalid index" << endl;
+        return -1;
+    }
+    Node *last = first;
+    for (int i = 0; i < index; i++)
+    {
+        last = last->next;
+    }
+    return last->data;
+}
+
+int LinkedList::getPreviousValue(int item)
+{
+    Node *last = first;
+    while (last->next->data != item)
+    {
+        last = last->next;
+    }
+    return last->data;
+}
+
+Node LinkedList::getPrevious(Node *node)
+{
+    Node 
+}
+
+int *LinkedList::toArray()
+{
+    int *ptr = new int[size];
+    Node *last = first;
+    int i = 0;
+    while (last != NULL)
+    {
+        ptr[i] = last->data;
+        i++;
+        last = last->next;
+    }
+    return ptr;
+}
+
 int main()
 {
-    Node *LinkedList = NULL;
-    insertAtLast(LinkedList, 10);
-    insertAtLast(LinkedList, 20);
-    insertAtLast(LinkedList, 30);
-    insertAtFirst(LinkedList, 5);
-    insertAtFirst(LinkedList, 4);
-    insertAtFirst(LinkedList, 3);
-    insertAtFirst(LinkedList, 2);
-    insertAtFirst(LinkedList, 1);
-    insertAtLast(LinkedList, 40);
-    display(LinkedList);
-    cout << exists(LinkedList, 50) << endl;
+    LinkedList newLinkedList;
+    // newLinkedList.insertAtLast(10);
+    newLinkedList.insertAtLast(20);
+    newLinkedList.insertAtLast(30);
+    newLinkedList.insertAtLast(40);
+    newLinkedList.insertAtLast(50);
+    newLinkedList.insertAtLast(60);
+    newLinkedList.insertAtLast(70);
+    newLinkedList.insertAtFirst(10);
+    // newLinkedList.insertAtFirst(10);
+    newLinkedList.display();
+    int *array = newLinkedList.toArray();
+    newLinkedList.displayArray(array);
 
     return 0;
 }
