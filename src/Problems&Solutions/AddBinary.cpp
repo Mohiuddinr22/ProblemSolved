111b9689a4e4fea1fe06ed5b3ced81906a5051c5
Mohiuddinr22 <mohiuddinr.22@gmail.com>
2023-03-09 18:50:14 +0600

    git init

--- src/Problems&Solutions/AddBinary.cpp
+++ src/Problems&Solutions/AddBinary.cpp
@@ -5,14 +5,41 @@ Given two binary strings a and b, return their sum as a binary string.
 #include <bits/stdc++.h>
 using namespace std;
 
+string reverseStr(string str)
+{
+    int n = str.length();
+    for (int i = 0; i < n / 2; i++)
+        swap(str[i], str[n - i - 1]);
+
+    return str;
+}
+
 string addBinary(string a, string b)
 {
-    int sum = 0, carry = 0, i = a.length() - 1, j = b.length() - 1;
+    int sum, carry = 0, i = a.length() - 1, j = b.length() - 1;
     string ans = "";
-    while (i >= 0 && j >= 0)
+    while (i >= 0 || j >= 0)
     {
-        if (a[i] == '0' && b[i] == '0')
+        sum = carry;
+        if (i >= 0)
+        {
+            sum += ((int)a[i] - 48);
+            i--;
+        }
+        if (j >= 0)
+        {
+            sum += ((int)b[j] - 48);
+            j--;
+        }
+        carry = sum / 2;
+        sum = sum % 2;
+        ans += sum + 48;
     }
+    if (carry == 1)
+        ans += '1';
+
+    reverse(ans.begin(), ans.end());
+    return ans;
 }
 
 int main()
