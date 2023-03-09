be10741311098f8b22e5a2cec7bed0db7d433226
Mohiuddinr22 <mohiuddinr.22@gmail.com>
2023-03-09 09:41:28 +0600

    git init

---
+++ src/Problems&Solutions/ReturnDuplicate.cpp
@@ -0,0 +1,24 @@
+/*
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findDuplicate(vector<int> &nums)
+{
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size() - 1; i++)
+    {
+        if (nums[i] == nums[i + 1])
+            return nums[i];
+    }
+    return -1;
+}
+
+int main() {}
