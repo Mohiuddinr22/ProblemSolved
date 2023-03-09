29fe0f9e4ff3a9c99154886408a3926e39279d19
Mohiuddinr22 <mohiuddinr.22@gmail.com>
2023-03-09 16:22:14 +0600

    git init

--- src/Problems&Solutions/AddBinary.cpp
+++ src/Problems&Solutions/AddBinary.cpp
@@ -2,13 +2,17 @@
 Given two binary strings a and b, return their sum as a binary string.
 */
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 string addBinary(string a, string b)
 {
-    int carry = 0;
-    
+    int sum = 0, carry = 0, i = a.length() - 1, j = b.length() - 1;
+    string ans = "";
+    while (i >= 0 && j >= 0)
+    {
+        if (a[i] == '0' && b[i] == '0')
+    }
 }
 
 int main()
--- src/Problems&Solutions/StringSubsequence.cpp
+++ src/Problems&Solutions/StringSubsequence.cpp
@@ -4,7 +4,7 @@ Given two strings s and t, return true if s is a subsequence of t, or false othe
 A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 */
 
-#include <bits/htdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 bool isSubsequence(string s, string t)
