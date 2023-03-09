01c40a90cf6b79be16f7f481b6fcdde48863510d
Mohiuddinr22 <mohiuddinr.22@gmail.com>
2023-03-09 14:49:06 +0600

    git init

--- src/Problems&Solutions/ReturnDuplicate.cpp
+++ src/Problems&Solutions/ReturnDuplicate.cpp
@@ -10,12 +10,59 @@ You must solve the problem without modifying the array nums and uses only consta
 #include <vector>
 using namespace std;
 
-int findDuplicate(vector<int> &nums)
+int findDuplicate(vector<int> &nums) // using sort method
 {
     sort(nums.begin(), nums.end());
     for (int i = 0; i < nums.size() - 1; i++)
     {
         if (nums[i] == nums[i + 1])
+            return nums[i];
+    }
+    return -1;
+}
+
+int findDuplicate2(vector<int> &nums) // using binary search method
+{
+    int low = 1, high = nums.size() - 1;
+    while (low < high)
+    {
+        int count = 0, mid = (high + low) / 2;
+        for (int i : nums)
+            if (i <= mid)
+                count++;
+        if (count <= mid)
+            low = mid + 1;
+        else
+            high = mid;
+    }
+    return low;
+}
+
+int findDuplicate3(vector<int> &nums) // using floyd's cycle detection method
+{
+    int fast = nums[0], slow = nums[0];
+    while (fast != slow)
+    {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    }
+    fast = nums[0];
+    while (fast != slow)
+    {
+        fast = nums[fast];
+        slow = nums[slow];
+    }
+    return slow;
+}
+
+int findDuplicate4(vector<int> &nums) // index marking
+{
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int index = nums[i] - 1;
+        index = index *= -1;
+
+        if (index > 0)
             return nums[i];
     }
     return -1;
