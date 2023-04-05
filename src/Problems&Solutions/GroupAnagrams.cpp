7a7434610761131f6f2669c622c908627f3fc1c9
Mohiuddinr22 <mohiuddinr.22@gmail.com>
2023-04-05 06:49:19 +0600

    git init

--- src/Problems&Solutions/GroupAnagrams.cpp
+++ src/Problems&Solutions/GroupAnagrams.cpp
@@ -24,18 +24,63 @@ using namespace std;
 
 class Solution
 {
+// private:
+//     void eraseWord(vector<string> &strs, string str)
+//     {
+//         auto it = find(strs.begin(), strs.end(), str);
+//         if (it != strs.end())
+//             strs.erase(it);
+//     }
+//     bool isAnagram(string s, string t)
+//     {
+//         sort(s.begin(), s.end());
+//         sort(t.begin(), t.end());
+//         if (s == t)
+//             return true;
+//         else
+//             return false;
+//     }
+
 public:
     vector<vector<string>> groupAnagrams(vector<string> &strs)
     {
-        vector<vector<string>> strings = strs;
+        unordered_map<string, vector<string>> map;
+        for (auto str : strs)
+        {
+            string s = str;
+            sort(s.begin(), s.end());
+            map[s].push_back(str);
+        }
         vector<vector<string>> answer;
-        for (int i = 0; i < strings.size(); i++)
-            for (int j = 0; j < strings[i].size(); j++)
-                sort(strings[i][j].begin(), strings[i][j].end());
-        for (int i = 0; i < strings.size(); i++)
-            for (int j = 0; i < strings.size(); j++)
-            {
-                answer[i].push_back([i][j]);
-            }
+        for (auto str : map)
+        {
+            answer.push_back(str.second);
+        }
+        return answer;
     }
+    // vector<vector<string>> groupAnagrams(vector<string> &strs)
+    // {
+    //     vector<vector<string>> ans;
+    //     int i = 0;
+    //     while (i < strs.size())
+    //     {
+    //         vector<string> str;
+    //         string s = strs[i];
+    //         str.push_back(strs[i]);
+    //         //eraseWord(strs, strs[i]);
+    //         int k = 0;
+    //         while (k < strs.size())
+    //         {
+    //             if (i != k && isAnagram(s, strs[k]))
+    //             {
+    //                 str.push_back(strs[k]);
+    //                 eraseWord(strs, strs[k]);
+    //             }
+    //             k++;
+    //         }
+    //         i++;
+    //         ans.push_back(str);
+    //     }
+    //     return ans;
+    // }
 };
